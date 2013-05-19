82a73bdd59640fe7ae67f9d5c27dedf89a16afd3
Johan Kotlinski <kotlinski@gmail.com>
2013-05-19 17:06:47 +0200

    Moved package to com.littlesounddj.lsdmanager

--- src/LSDSavFile.java
+++ src/LSDSavFile.java
@@ -18,6 +18,8 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE. */
 
+package com.littlesounddj.lsdmanager;
+
 import java.io.RandomAccessFile;
 import javax.swing.JList;
 import javax.swing.JTable;
